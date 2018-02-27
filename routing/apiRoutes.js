908060edea5df60cefc3ad4c953d1c005e5f60fe
Eric Traband <ejstraband@gmail.com>
2018-02-26 20:01:04 -0600

    monday start

---
+++ routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+module.exports = function(app) {
+
+app.get("/api/friends", function(req, res) {
+    res.json(friends);
+});
+
+app.post("/api/friends", function(req, res) {
+    res.json(friends);
+});
+
+};
