b368a08eab7eb74c1d4065bcc754a8c9391ca37e
Eric Traband <ejstraband@gmail.com>
2018-03-02 22:00:17 -0600

    trying heroku

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -12,7 +12,7 @@ app.get("/api/friends", function(req, res) {
 app.post("/api/newFriend", function(req, res) {
     var newFriend = req.body;
     // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newFriend);
+    // console.log(newFriend);
     friends.push(newFriend);
     res.json(newFriend);
     });
